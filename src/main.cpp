0feed335e93c885a68964502cd07334d8d522307
nebula-INE <sekoia-ine@proton.me>
2026-08-16 11:34:25 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
-#include <iostream>
+extern "C" {
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
+}
+
+#include <iostream>
 
 int main() {
     std::cout << "aural-engine starting...\n";
     std::cout << "FFmpeg version: " << av_version_info() << std::endl;
-
-    // 後でここにデコード処理を書く
     return 0;
 }
